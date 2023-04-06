5f6656ec402e3a6703d398fe5676392cf4161b91
Furkan Çağlayan <furkancaglayan15@hotmail.com>
2023-04-06 23:56:39 +0300

    format.

--- src/mix/core/mixWindow.cpp
+++ src/mix/core/mixWindow.cpp
@@ -1,86 +1,88 @@
 #include "mixWindow.h"
-#include <stdio.h>
 #include <iostream>
+#include <stdio.h>
 
-int mix::core::mixWindow::get_key(int key) noexcept
+int mix::core::mixWindow::get_key (int key) noexcept
 {
-    return glfwGetKey(_glfw_window, key);
+    return glfwGetKey (_glfw_window, key);
 }
 
-void mix::core::mixWindow::set_window_mode(WindowMode mode)
+void mix::core::mixWindow::set_window_mode (WindowMode mode)
 {
     _mode = mode;
     if (mode == WindowMode::Windowed)
     {
-        glfwSetWindowMonitor(_glfw_window, NULL, _x, _y, _width, _height, 0);
+        glfwSetWindowMonitor (_glfw_window, NULL, _x, _y, _width, _height, 0);
     }
     else if (mode == WindowMode::FullScreen)
     {
-        const GLFWvidmode* _mode = glfwGetVideoMode(_monitor);
-        glfwSetWindowMonitor(_glfw_window, _monitor, 0, 0, _mode->width, _mode->height, _mode->refreshRate);
+        const GLFWvidmode* _mode = glfwGetVideoMode (_monitor);
+        glfwSetWindowMonitor (_glfw_window, _monitor, 0, 0, _mode->width,
+                              _mode->height, _mode->refreshRate);
     }
 }
 
-void mix::core::mixWindow::set_monitor(GLFWmonitor* monitor)
+void mix::core::mixWindow::set_monitor (GLFWmonitor* monitor)
 {
     if (_mode == WindowMode::FullScreen)
     {
         _monitor = monitor;
-        const GLFWvidmode* mode = glfwGetVideoMode(_monitor);
+        const GLFWvidmode* mode = glfwGetVideoMode (_monitor);
         if (!mode)
         {
-            glfwTerminate();
-            printf("Cant retrieve mode");
+            glfwTerminate ();
+            printf ("Cant retrieve mode");
         }
 
-        glfwWindowHint(GLFW_RED_BITS, mode->redBits);
-        glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
-        glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
-        glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+        glfwWindowHint (GLFW_RED_BITS, mode->redBits);
+        glfwWindowHint (GLFW_GREEN_BITS, mode->greenBits);
+        glfwWindowHint (GLFW_BLUE_BITS, mode->blueBits);
+        glfwWindowHint (GLFW_REFRESH_RATE, mode->refreshRate);
 
-        glfwSetWindowMonitor(_glfw_window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+        glfwSetWindowMonitor (_glfw_window, monitor, 0, 0, mode->width,
+                              mode->height, mode->refreshRate);
     }
     else if (_mode == WindowMode::Windowed)
     {
-        glfwSetWindowMonitor(_glfw_window, NULL, _x, _y, _width, _height, 0);
+        glfwSetWindowMonitor (_glfw_window, NULL, _x, _y, _width, _height, 0);
     }
 }
 
-GLFWwindow* mix::core::mixWindow::get_glfw_window() const noexcept
+GLFWwindow* mix::core::mixWindow::get_glfw_window () const noexcept
 {
     return _glfw_window;
 }
 
-void mix::core::mixWindow::set_context_as_current() noexcept
+void mix::core::mixWindow::set_context_as_current () noexcept
 {
-    glfwMakeContextCurrent(_glfw_window);
+    glfwMakeContextCurrent (_glfw_window);
 }
 
-mix::core::mixWindow::~mixWindow()
+mix::core::mixWindow::~mixWindow ()
 {
 }
 
-inline void mix::core::mixWindow::close() noexcept
+inline void mix::core::mixWindow::close () noexcept
 {
-    glfwDestroyWindow(_glfw_window);
+    glfwDestroyWindow (_glfw_window);
 }
 
-void mix::core::mixWindow::initialize() noexcept
+void mix::core::mixWindow::initialize () noexcept
 {
     std::cout << "Creating Window" << std::endl;
 
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, _context_version_max);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, _context_version_min);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, _context_version_max);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, _context_version_min);
+    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    _glfw_window = glfwCreateWindow(_width, _height, "GLFW: Window created", NULL, NULL);
+    _glfw_window = glfwCreateWindow (_width, _height, "GLFW: Window created", NULL, NULL);
     if (!_glfw_window)
     {
-        glfwTerminate();
-        printf("Failed to create a window");
+        glfwTerminate ();
+        printf ("Failed to create a window");
     }
-    set_monitor(glfwGetPrimaryMonitor());
+    set_monitor (glfwGetPrimaryMonitor ());
 
-    set_context_as_current();
+    set_context_as_current ();
 }
--- src/mix/main.cpp
+++ src/mix/main.cpp
@@ -1,49 +1,51 @@
-#include <iostream>
-#include <GL/glew.h>
-#include <memory>
-#include <imgui.h>
 #include "editor/mixEditor.h"
-#include <stb_image.h>
+#include <GL/glew.h>
 #include <glm/glm.hpp> // vec2, vec3, mat4, radians
+#include <imgui.h>
+#include <iostream>
+#include <memory>
+#include <stb_image.h>
 
 // Include all GLM extensions
 #include <glm/ext.hpp> // perspective, translate, rotate
 
-glm::mat4 transform(glm::vec2 const& Orientation, glm::vec3 const& Translate, glm::vec3 const& Up)
+glm::mat4 transform (glm::vec2 const& Orientation, glm::vec3 const& Translate, glm::vec3 const& Up)
 {
-	glm::mat4 Proj = glm::perspective(glm::radians(45.f), 1.33f, 0.1f, 10.f);
-	glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.f), Translate);
-	glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Orientation.y, Up);
-	glm::mat4 View = glm::rotate(ViewRotateX, Orientation.x, Up);
-	glm::mat4 Model = glm::mat4(1.0f);
-	return Proj * View * Model;
+    glm::mat4 Proj = glm::perspective (glm::radians (45.f), 1.33f, 0.1f, 10.f);
+    glm::mat4 ViewTranslate = glm::translate (glm::mat4 (1.f), Translate);
+    glm::mat4 ViewRotateX = glm::rotate (ViewTranslate, Orientation.y, Up);
+    glm::mat4 View = glm::rotate (ViewRotateX, Orientation.x, Up);
+    glm::mat4 Model = glm::mat4 (1.0f);
+    return Proj * View * Model;
 }
 
-int main() 
+int main ()
 {
-	int width, height, channels;
-	     unsigned char* img = stbi_load("sky.jpg", &width, &height, &channels, 0);
-	     if (img == NULL) {
-		        printf("Error in loading the image\n");
-		        exit(1);
-		
-	}
-	printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
+    int width, height, channels;
+    unsigned char* img = stbi_load ("sky.jpg", &width, &height, &channels, 0);
+    if (img == NULL)
+    {
+        printf ("Error in loading the image\n");
+        exit (1);
+    }
+    printf ("Loaded image with a width of %dpx, a height of %dpx and %d "
+            "channels\n",
+            width, height, channels);
 
-	GLenum err;
-	if (!glfwInit())
-	{
-		std::cout << "GLFW INIT ERROR!";
-	}
-	glewExperimental = true;
-	if ((err = glewInit()) != GLEW_OK)
-	{
-		std::cout << glewGetErrorString(err) << std::endl;
-	}
+    GLenum err;
+    if (!glfwInit ())
+    {
+        std::cout << "GLFW INIT ERROR!";
+    }
+    glewExperimental = true;
+    if ((err = glewInit ()) != GLEW_OK)
+    {
+        std::cout << glewGetErrorString (err) << std::endl;
+    }
 
-	mix::mixEditor::create_new();
-	while (mix::mixEditor::_instance->_should_run)
-	{
-		mix::mixEditor::_instance->run();
-	}
+    mix::mixEditor::create_new ();
+    while (mix::mixEditor::_instance->_should_run)
+    {
+        mix::mixEditor::_instance->run ();
+    }
 }
